e43160d578c63e44696dace14e75036a9b755cd3
Camilo <camilocastelar3@gmail.com>
2025-09-18 01:21:23 -0500

    Domain completo - User Entity - UserRepository

--- src/Domain/User/UserRepository.php
+++ src/Domain/User/UserRepository.php
@@ -1,1 +1,16 @@
 <?php
+
+declare(strict_types=1);
+
+namespace App\Domain\User;
+
+interface UserRepository
+{
+    public function save(User $user): void;
+    public function findById(string $id): ?User;
+    public function findByEmail(string $email): ?User;
+    /** @return User[] */
+    public function findAll(): array;
+    public function update(User $user): void;
+    public function delete(string $id): void;
+}
